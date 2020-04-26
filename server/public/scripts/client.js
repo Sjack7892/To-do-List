a8241a1fea64d04264a50c96a6c506f168d36bc1
Steve Jackson <sjack7892@gmail.com>
2020-04-26 00:18:53 -0500

    GET working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,29 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('jquery ready');
+    getList();
+}
+
+function getList() {
+    console.log('in getTodoList');
+    $.ajax({
+        type: 'GET',
+        url: '/todo',
+    }).then(function(response) {
+        console.log('in /todo GET:', response);
+        appendList(response);
+    }).catch(function(error) {
+        console.log('error in /todo GET:', error);
+    });
+}
+
+function appendList(response) {
+    console.log(response);
+    let el = $('#list');
+    el.empty();
+    for (let i = 0; i < response.length; i++) {
+        el.append(`
+        <li>${response[i].task}</li>
+        `);
+    }
 }
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -1,15 +1,38 @@
 const express = require('express');
-const koalaRouter = express.Router();
+const todoRouter = express.Router();
 
 // DB CONNECTION
 const pool = require('../modules/pool');
 
+let stuff = ['fish', 'taco', 'beans', 'baseball'];
+
+// koalaRouter.get('/', (req, res) => {
+//     let queryText = 'SELECT * FROM "koalas" ORDER BY "id" DESC;';
+//     pool.query(queryText).then(result => {
+//         res.send(result.rows);
+//     })
+//         .catch(error => {
+//             console.log('error getting koalas', error);
+//             res.sendStatus(500);
+//         });
+// });
 
 // GET
-
-// PUSH
+todoRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todo" ORDER BY "id";';
+    pool.query(queryText).then(result => {
+        console.log(result.rows);
+        res.send(result.rows);
+    })
+        .catch(error => {
+            console.log('error getting todo', error);
+            res.sendStatus(500);
+        })
+});
 
 // POST
+
+// PUT
 
 // DELETE
 
